adc1cb1a9792fb3f8c717451aeabb3988c8f17ea
git-SakshiManjari98 <sakshimanjari@gmail.com>
2021-03-22 15:16:48 +0530

    b

--- jenkinsnew1/Program.cs
+++ jenkinsnew1/Program.cs
@@ -11,7 +11,7 @@ namespace jenkinsnew1
         static void Main(string[] args)
         {
             Console.WriteLine("hi");
-            Console.ReadLine();
+            Console.ReadLine()
         }
     }
 }
